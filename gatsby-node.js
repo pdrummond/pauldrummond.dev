1613c5a5c0bbd883b94632d83261e5e53e717081
Paul Drummond <paul.drummond@iode.co.uk>
2019-10-20 13:46:10 +0100

    Added supprot for draft posts.

--- gatsby-node.js
+++ gatsby-node.js
@@ -41,3 +41,30 @@ exports.createPages = ({ graphql, actions }) => {
     })
   })
 }
+
+exports.createSchemaCustomization = ({ actions, schema }) => {
+  const { createTypes, createFieldExtension } = actions
+
+  createFieldExtension({
+    name: `defaultFalse`,
+    extend() {
+      return {
+        resolve(source, args, context, info) {
+          if (source[info.fieldName] == null) {
+            return false
+          }
+          return source[info.fieldName]
+        },
+      }
+    },
+  })
+
+  createTypes(`
+    type MarkdownRemark implements Node {
+      frontmatter: Frontmatter
+    }
+    type Frontmatter {
+      draft: Boolean @defaultFalse
+    }
+  `)
+}
